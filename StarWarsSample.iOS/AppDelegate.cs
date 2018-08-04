8e5f0d3fb16648f6c18f09695302931741ba5543
rspl-test <42083839+rspl-test@users.noreply.github.com>
2018-08-04 10:53:08 +0530

    Add files via upload
    IOS

---
+++ StarWarsSample.iOS/AppDelegate.cs
@@ -0,0 +1,51 @@
+﻿using Foundation;
+using MvvmCross.Platforms.Ios.Core;
+using MvvmCross.Plugin.Color.Platforms.Ios;
+using StarWarsSample.Core;
+using UIKit;
+
+namespace StarWarsSample.iOS
+{
+    // The UIApplicationDelegate for the application. This class is responsible for launching the
+    // User Interface of the application, as well as listening (and optionally responding) to application events from iOS.
+    [Register("AppDelegate")]
+    public partial class AppDelegate : MvxApplicationDelegate<Setup, App>
+    {
+        public override bool FinishedLaunching(UIApplication application, NSDictionary launchOptions)
+        {
+            var result = base.FinishedLaunching(application, launchOptions);
+
+            CustomizeAppearance();
+
+            return result;
+        }
+
+        private void CustomizeAppearance()
+        {
+            UINavigationBar.Appearance.SetBackgroundImage(new UIImage(), UIBarPosition.Any, UIBarMetrics.Default);
+            UINavigationBar.Appearance.ShadowImage = new UIImage();
+
+            UINavigationBar.Appearance.SetTitleTextAttributes(new UITextAttributes()
+            {
+                TextColor = UIColor.White,
+                Font = UIFont.SystemFontOfSize(17f, UIFontWeight.Semibold)
+            });
+            UINavigationBar.Appearance.Translucent = false;
+            UINavigationBar.Appearance.BarTintColor = AppColors.PrimaryColor.ToNativeColor();
+            UINavigationBar.Appearance.TintColor = UIColor.White;
+            UINavigationBar.Appearance.BackgroundColor = AppColors.PrimaryColor.ToNativeColor();
+            UINavigationBar.Appearance.BackIndicatorImage = new UIImage();
+
+            UITabBar.Appearance.BackgroundColor = AppColors.PrimaryColor.ToNativeColor();
+            UITabBarItem.Appearance.SetTitleTextAttributes(new UITextAttributes()
+            {
+                TextColor = AppColors.AccentColor.ToNativeColor()
+            }, UIControlState.Selected);
+
+            UITextField.Appearance.TintColor = AppColors.AccentColor.ToNativeColor();
+            UITextView.Appearance.TintColor = AppColors.AccentColor.ToNativeColor();
+            UIButton.Appearance.SetTitleColor(AppColors.AccentColor.ToNativeColor(), UIControlState.Highlighted);
+        }
+    }
+}
+
---
+++ StarWarsSample.iOS/Setup.cs
@@ -0,0 +1,38 @@
+﻿using MvvmCross;
+using MvvmCross.Base;
+using MvvmCross.Binding.Bindings.Target.Construction;
+using MvvmCross.IoC;
+using MvvmCross.Platforms.Ios.Core;
+using MvvmCross.Plugin.Json;
+using StarWarsSample.Core;
+using StarWarsSample.iOS.MvxBindings;
+using UIKit;
+
+namespace StarWarsSample.iOS
+{
+    public class Setup : MvxIosSetup<App>
+    {
+        protected override void InitializeFirstChance()
+        {
+            base.InitializeFirstChance();
+
+            Mvx.RegisterType<IMvxJsonConverter, MvxJsonConverter>();
+        }
+
+        protected override void InitializeLastChance()
+        {
+            base.InitializeLastChance();
+
+            var registry = Mvx.Resolve<IMvxTargetBindingFactoryRegistry>();
+            registry.RegisterFactory(new MvxCustomBindingFactory<UIViewController>("NetworkIndicator", (viewController) => new NetworkIndicatorTargetBinding(viewController)));
+        }
+
+        protected override IMvxIocOptions CreateIocOptions()
+        {
+            return new MvxIocOptions
+            {
+                PropertyInjectorOptions = MvxPropertyInjectorOptions.MvxInject
+            };
+        }
+    }
+}
